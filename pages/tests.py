df6bde30dc00073be54fb933aafe8c3a33d21023
Slava Pavlutin <sl.pavlutin@gmail.com>
2015-09-21 18:54:48 +0300

    Add tests for  app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from . import views
+
+
+class PagesTest(TestCase):
+  def test_root_url_resolves_to_index_view(self):
+    view = resolve('/')
+    self.assertEqual(view.func, views.index)
+
+  def test_about_url_resolves_to_about_view(self):
+    view = resolve('/about/')
+    self.assertEqual(view.func, views.about)
