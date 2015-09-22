9333a67fe72eaa3f99bfdff840736ec5ad88eb49
Slava Pavlutin <sl.pavlutin@gmail.com>
2015-09-22 07:12:07 +0300

    Add tests for registration and User

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,47 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.template.loader import render_to_string
+from django.contrib.auth import authenticate
+
+from .models import User
+
+
+class UserTest(TestCase):
+  # TODO: Add contact information.
+  # TODO: Email should be unique.
+  def setUp(self):
+    self.user = User.objects.create()
+
+  def test_get_absolute_url(self):
+    url = self.user.get_absolute_url()
+    self.assertEqual(url, '/user/1/')
 
 
 class RegisterTest(TestCase):
+  def setUp(self):
+    self.user = User.objects.create(username='user')
+    self.user.set_password('password')
+    self.user.save()
+    self.valid_user_data = {
+      'username': 'slava',
+      'email': 'user@mail.com',
+      'password': 'password',
+      'password_confirmation': 'password',
+    }
+
+  # TODO: Redirect to user profile if authenticated.
   def test_visitors_can_register(self):
     response = self.client.get(reverse('users:register'))
     self.assertTemplateUsed(response, 'users/register.html')
+
+  def test_redirects_if_authenticated(self):
+    self.client.login(username=self.user.username, password='password')
+    response = self.client.get(reverse('users:register'))
+    self.assertRedirects(response, 'user/1/')
+
+  def test_registration(self):
+    response = self.client.post(reverse('users:register'), self.valid_user_data)
+    self.assertRedirects(response, 'user/2/', target_status_code=302)
 
   def test_missing_email(self):
     data = {
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from .models import User
 from .forms import UserCreationForm
+from . import anonymous_required
 
 
 @login_required(login_url='users:login')
@@ -15,3 +17,6 @@ def user(request, id):
 class Register(CreateView):
   template_name = 'users/register.html'
   form_class = UserCreationForm
+  
+  get = method_decorator(anonymous_required)(CreateView.get)
+  post = method_decorator(anonymous_required)(CreateView.post)
