f52f72eed33b90c4748e500d666259eb4eefdf86
Slava Pavlutin <sl.pavlutin@gmail.com>
2015-09-24 01:21:17 +0300

    Refactor users/tests to test for specific error messages

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -14,28 +14,32 @@ class UserCreationFormTest(TestCase):
     }
 
   def test_creating_user(self):
-    form = UserCreationForm(data=self.valid_data)
+    form = self._create_form(self.valid_data)
     self.assertIsInstance(form.save(), User)
 
   def test_username_unique(self):
     self._create_user(self.valid_data)
-    form = UserCreationForm(data=self.valid_data)
-    self.assertRaises(ValueError, lambda: form.save())
+    form = self._create_form(self.valid_data)
+    self.assertIn('A user with that username already exists.',
+                   form.errors['username'])
 
   def test_email_required(self):
     self.valid_data.pop('email')
-    form = UserCreationForm(data=self.valid_data)
-    self.assertRaises(ValueError, lambda: form.save())
+    form = self._create_form(self.valid_data)
+    self.assertIn('Email is required.', form.errors['email'])
 
   def test_username_required(self):
     self.valid_data.pop('username')
-    form = UserCreationForm(data=self.valid_data)
-    self.assertRaises(ValueError, lambda: form.save())
+    form = self._create_form(self.valid_data)
+    self.assertIn('This field is required.', form.errors['username'])
 
   def test_password_required(self):
     self.valid_data.pop('password')
-    form = UserCreationForm(data=self.valid_data)
-    self.assertRaises(ValueError, lambda: form.save())
+    form = self._create_form(self.valid_data)
+    self.assertIn('This field is required.', form.errors['password'])
+
+  def _create_form(self, data):
+    return UserCreationForm(data=data)
 
   def _create_user(self, data):
     user = User.objects.create(username=data['username'])
